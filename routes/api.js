60217e8437a1eee78bf53df4eae34a52eb9e6c2d
Sam Scheers Cardona <scheers@bk.ru>
2024-09-09 17:54:25 +0200

    fix: rewrite api

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
 
   app.route('/api/issues/:project')
 
+    // GET route for viewing issues with optional filters
     .get(function (req, res) {
       let project = req.params.project;
       let filters = req.query;
@@ -15,9 +16,11 @@ module.exports = function (app) {
         return Object.keys(filters).every(key => issue[key] == filters[key]);
       });
 
+      // Return the filtered issues
       res.json(filteredIssues);
     })
 
+    // POST route for creating a new issue
     .post(function (req, res) {
       let project = req.params.project;
       const { issue_title, issue_text, created_by, assigned_to = '', status_text = '' } = req.body;
@@ -51,10 +54,12 @@ module.exports = function (app) {
       res.json(newIssue);
     })
 
+    // PUT route for updating an existing issue
     .put(function (req, res) {
       let project = req.params.project;
       let { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = req.body;
 
+      // Check if _id is provided
       if (!_id) {
         return res.json({ error: 'missing _id' });
       }
@@ -62,11 +67,20 @@ module.exports = function (app) {
       let projectIssues = issues[project] || [];
       let issue = projectIssues.find(issue => issue._id === _id);
 
+      // If issue is not found, return an error
       if (!issue) {
         return res.json({ error: 'could not update', _id });
       }
 
-      // Update fields if they are provided
+      // Check if there are fields to update
+      const fieldsToUpdate = { issue_title, issue_text, created_by, assigned_to, status_text, open };
+      const hasUpdates = Object.keys(fieldsToUpdate).some(field => fieldsToUpdate[field] !== undefined && fieldsToUpdate[field] !== '');
+
+      if (!hasUpdates) {
+        return res.json({ error: 'no update field(s) sent', _id });
+      }
+
+      // Update the fields if they are provided
       issue.issue_title = issue_title || issue.issue_title;
       issue.issue_text = issue_text || issue.issue_text;
       issue.created_by = created_by || issue.created_by;
@@ -78,10 +92,12 @@ module.exports = function (app) {
       res.json({ result: 'successfully updated', _id });
     })
 
+    // DELETE route for deleting an issue
     .delete(function (req, res) {
       let project = req.params.project;
       let { _id } = req.body;
 
+      // Check if _id is provided
       if (!_id) {
         return res.json({ error: 'missing _id' });
       }
@@ -89,13 +105,14 @@ module.exports = function (app) {
       let projectIssues = issues[project] || [];
       let index = projectIssues.findIndex(issue => issue._id === _id);
 
+      // If issue is not found, return an error
       if (index === -1) {
         return res.json({ error: 'could not delete', _id });
       }
 
+      // Delete the issue
       projectIssues.splice(index, 1);
 
       res.json({ result: 'successfully deleted', _id });
     });
-
 };
